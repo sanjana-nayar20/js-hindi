7dae8fad389656d6c2196bcf653f28c49afbc16c
sanjana-nayar20 <nayarsanjana5@gmail.com>
2025-08-10 14:41:47 +0000

    add changes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,48 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber)
+
+// 33 - number
+// 33abc // nan - not a number
+//null - 0
+// undefined - nan
+// true - 1
+
+let isLoggedIn = 'sss'
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 - true 
+// 0 - false
+// '' - false
+// 'ss' = true
+
+//  ****************** operations ******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let s1 = "a"
+let s2 = " b"
+
+let s3 = s1 + s2
+// console.log(s3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1+2+"2")
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3
+// num1=num2=num3 = 2+2
+
+let gc = 100
+gc++
+console.log(gc);
