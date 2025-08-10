e14e46386dbbedad33ede3985adc1ac9b4f6a718
sanjana-nayar20 <nayarsanjana5@gmail.com>
2025-08-10 05:34:11 +0000

    add it

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version 
+//  alert(3+3) //we r using node js not browser.
+
+console.log(3
+    +3); // code reablity is necessary.
+console.log("sss")
+
+// number 2 to power 53
+//  bigint
+// string - " "
+//  boolean - true/false
+// null - standalone value
+// undefined  - null or not assigned
+// symbol - unique
+
+// object 
+
+console.log(typeof "sanj")
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553 
+let accountEmail = "sanj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 
+accountEmail = "sanjananja"
+accountPassword = "sasasa"
+console.table([accountEmail,accountPassword,accountCity])
+
+/*
+prefer not to use var 
+bcoz of issue in block scope n functional scope
+*/
