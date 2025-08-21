56b8b8b5e88fec1f05d4e4192dbca18f4dd0882c
sanjana-nayar20 <nayarsanjana5@gmail.com>
2025-08-21 12:30:14 +0000

    add

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,7 +9,40 @@ function myFunc(){
 }
 myFunc()
 
+// function addTwoNum(num1,num2){
+
+//     console.log(num1+num2); 
+// }
+// addTwoNum(2,2)
+
 function addTwoNum(num1,num2){
-    console.log(num1+num2); 
+    return num1+num2;
+    
 }
-addTwoNum(2,2)
+const result = addTwoNum(9,1)
+// console.log("result",result);
+
+function logIn(username) {
+    return `${username} just logged in`
+}
+// console.log(logIn("sanjana"));
+
+function calculateCartPrice(val1,va2,...num1) {
+    return num1
+    
+}
+console.log(calculateCartPrice(200,300,400,2000))
+
+const user = {
+    username:"sanj",
+    price:444
+}
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} n price is ${anyobject.price}`);
+}
+handleObject(user) 
+const myNewArray = [10,20,30,300]
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
