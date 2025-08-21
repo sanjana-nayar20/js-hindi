8f55b0713cc47a15112781e863360b774c4acef8
sanjana-nayar20 <nayarsanjana5@gmail.com>
2025-08-21 11:57:07 +0000

    add

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,11 +1,15 @@
 function myFunc(){
     console.log("s");
-    console.log("s")
-    console.log("s")
-    console.log("s")
-    console.log("s")
-    console.log("s")
-    console.log("s")
-
-
+    console.log("a");
+    console.log("n");
+    console.log("j");
+    console.log("a");
+    console.log("n");
+    console.log("a");
 }
+myFunc()
+
+function addTwoNum(num1,num2){
+    console.log(num1+num2); 
+}
+addTwoNum(2,2)
