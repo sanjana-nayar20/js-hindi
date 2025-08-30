6fb263ddb2620e5503d02c925260c0c895eee216
sanjana-nayar20 <nayarsanjana5@gmail.com>
2025-08-30 11:55:36 +0000

    add

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -5,6 +5,8 @@
     console.log(`DB connected`);    
 })();
 
+//iife is used -> prevent global scope pollution
+
 //unnamaed iife
 ((name) => {
     console.log(`DB connected 2`);
---
+++ 04_control_flow/one.js
@@ -0,0 +1,44 @@
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+//if
+
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if (2===2) {
+//     console.log("executed");
+// }
+
+
+//if else
+
+//if (temperature === 41) {
+//     console.log("executed");
+// }else{
+//     console.log("greater thean 50");
+// }
+
+const score = 300
+
+if(score > 100) {
+    var power = "fly"
+    console.log(`User power : ${power}`);
+}
+console.log(`User power : ${power}`);
+
+
+const balance = 1000
+
+// if (balance>500) console.log("test"),console.log("test2");
+
+if (balance>250) {
+    console.log("less than");  
+} else if (balance>500) {
+    console.log("less than");
+}
+else if (balance>800) {
+  console.log("less than");    
+ } else {
+    console.log("great than");    
+}
